bf1238d4d67f76a3f1729a8b830d1418daa249d3
leaena <awkward.lion@gmail.com>
2013-11-30 14:13:21 -0800

    Updated 22 and 23.

--- 22meanmode.js
+++ 22meanmode.js
@@ -1,26 +1,20 @@
 function MeanMode(arr) {
-  var mode;
-  var modeCount = [];
-  var count = 0;
-  var sum = 0;
+  var modeCount = {};                //object to hold numbers and their counts
+  var mode;                          //creating a variable for future mode
+  var mean;                          //creating a variable for future mean
+  var sum = 0;                       //initialzing a sum count
 
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    if(!modeCount[arr[i]-1]){
-      modeCount[arr[i]-1] = 0;
-    }
-    modeCount[arr[i]-1] += 1;
+  for ( var i = 0; i < arr.length; i++ ) { //cycling through the array of numbers
+    sum += arr[i];                   //adding numbers together for the full sum
+    modeCount[arr[i]] = modeCount[arr[i] || 0; //initialzing a key value pair or using the current one if we've seen the number before
+    modeCount[arr[i]] += 1;          //adding for the count of the number
   }
-  for(i = 0; i < modeCount.length; i++) {
-    if(modeCount[i] > count) {
-      mode = i+1;
-      count = modeCount[i];
+  mean = sum/arr.length;             //calculating the mean value
+  for( var key in modeCount ){       //looping through the modeCount
+    if( modeCount[key] > count){     //check the number of times a number has been seen
+      mode = parseInt(key);          //if its a new highest, convert the key back into a number and set it as the current mode
+      count = modeCount[key];        //set a new high count
     }
   }
-  var mean = sum/arr.length;
-  if (mean === mode) {
-    return 1;
-  } else {
-    return 0;
-  }     
+  return mode === mean;              //check to see if the mode and mean are equal
 }
--- 23dashinsert.js
+++ 23dashinsert.js
@@ -1,14 +1,12 @@
 function DashInsert(num) {
-  var num = num.toString().split("");
-  var result = [];
-  for(var i = 0; i < num.length; i++) {
-    result.push(num[i]);
-    if (num[i] % 2 !== 0){
-      if (num[i+1] % 2 === 1){
-        result.push("-");
-      }
+  var num = num.toString()                      //convert the number to a string
+               .split("");                      //split the string into an array of characters
+  var result = [];                              //create an empty array to hold our modified values
+  for(var i = 0; i < num.length; i++) {         //loop through our number array
+    result.push(num[i]);                        //add each number to our result array
+    if(num[i] % 2 !== 0 && num[i+1] % 2 === 1){ //check to see if the current number and then next number are odd
+      result.push("-");                         //push a dash if they are both odd so there is a dash between the two
     }
-  }
-                  
-  return result.join("");        
+  }               
+  return result.join("");                       //join the result array together into a string and return it        
 }
